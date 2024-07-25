26755e361796125ead8e959e4fcc73d340f0f744
di4rm4d4 <mcdermott.two@gmail.com>
2024-07-25 19:44:55 +0200

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -72,9 +72,9 @@ def classify_images(images_dir, results_dic, model):
 
       model_label = model_label.lower().strip()
 
-      pet_label = results_dic[key][0]
+      true = results_dic[key][0]
 
-      if pet_label in model_label:
+      if true in model_label:
         results_dic[key].extend([model_label, 1])
       else:
         results_dic[key].extend([model_label, 0])
