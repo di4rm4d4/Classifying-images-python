056b9d899e5e9f13fbc1b38a655e7c76a94d8e05
di4rm4d4 <mcdermott.two@gmail.com>
2024-07-25 18:16:06 +0200

    Update calculates_results_stats.py

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -95,8 +95,7 @@ def calculates_results_stats(results_dic):
                         results_stats_dic['n_correct_dogs'] += 1
 
         else:
-                if results_dic[key][3] != 1 and results_dic[key][4] != 1:
-                        results_stats_dic['n_correct_notdogs'] += 1
+            results_stats_dic['n_correct_notdogs'] += 1
 
     results_stats_dic['n_images'] = len(results_dic)
     results_stats_dic['n_notdogs_img'] = (results_stats_dic['n_images'] - results_stats_dic['n_dogs_img'])
